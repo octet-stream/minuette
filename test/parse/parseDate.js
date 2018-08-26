e4850ea6a5b029fdc026d1b725cf324e62e83acf
Nick K <nick.kruchinin@gmail.com>
2018-08-27 02:02:17 +0300

    Fixes for parseDay. Add first tests for parseDate.

---
+++ test/parse/parseDate.js
@@ -0,0 +1,47 @@
+const test = require("ava")
+
+const parseDate = require("../../lib/parse/parseDate")
+
+test("Should correctly parse any group of months", t => {
+  t.plan(3)
+
+  const feb = parseDate("11 Feb 2018")
+  const days30 = parseDate("14 Mar 2019")
+  const days31 = parseDate("25 Jun 2020")
+
+  t.deepEqual(feb, [11, "february", 2018])
+  t.deepEqual(days30, [14, "march", 2019])
+  t.deepEqual(days31, [25, "june", 2020])
+})
+
+test("Should return null on executing without any arguments", t => {
+  t.plan(1)
+
+  t.is(parseDate(), null)
+})
+
+test(
+  "Should throw a RangeError when days of february more then 28 " +
+  "in non-leap year",
+  t => {
+    t.plan(3)
+
+    const trap = () => parseDate("29 february 2018")
+
+    const err = t.throws(trap)
+
+    t.true(err instanceof RangeError)
+    t.is(err.message, "February have only 28 years in non-leap year.")
+  }
+)
+
+test("Should throw a TypeError when given value is not a string", t => {
+  t.plan(3)
+
+  const trap = () => parseDate([42])
+
+  const err = t.throws(trap)
+
+  t.true(err instanceof TypeError)
+  t.is(err.message, "The given value must be a string.")
+})

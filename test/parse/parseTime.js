ed4afe3cefe599a1b31d345fb046976d76b1111b
Nick K <nick.kruchinin@gmail.com>
2018-08-26 03:42:28 +0300

    Improve parseTime function. Add the first draft of parseDate function and setTime utility. Add isNaN utility because current ESLint config won't allow to use global isNaN function. Also, add an utility to check if given year is a leap in Gregorian calendar.

--- test/parse/parseTime.js
+++ test/parse/parseTime.js
@@ -5,7 +5,7 @@ const parseTime = require("../../lib/parse/parseTime")
 test("Should just parse time in 24h format", t => {
   t.plan(1)
 
-  const expected = ["00", "00", "00"]
+  const expected = [0, 0, 0]
 
   t.deepEqual(parseTime("00:00:00"), expected)
 })
@@ -13,7 +13,7 @@ test("Should just parse time in 24h format", t => {
 test("Should just parse a time in 12h format", t => {
   t.plan(1)
 
-  const expected = ["12", "58", "32", "am"]
+  const expected = [12, 58, 32, "am"]
 
   t.deepEqual(parseTime("12:58:32am"), expected)
 })
@@ -21,7 +21,7 @@ test("Should just parse a time in 12h format", t => {
 test("Should allow space(s) between time and am/pm mark", t => {
   t.plan(1)
 
-  const expected = ["12", "58", "32", "am"]
+  const expected = [12, 58, 32, "am"]
 
   t.deepEqual(parseTime("12:58:32          am"), expected)
 })
@@ -45,11 +45,11 @@ test("Should not parse incorrect time in both formats", t => {
 test("Should allow to omit MM and SS units in both formats", t => {
   t.plan(4)
 
-  t.deepEqual(parseTime("01:00am"), ["01", "00", undefined, "am"])
-  t.deepEqual(parseTime("00:00"), ["00", "00", undefined])
+  t.deepEqual(parseTime("01:00am"), [1, 0, undefined, "am"])
+  t.deepEqual(parseTime("00:00"), [0, 0, undefined])
 
-  t.deepEqual(parseTime("01am"), ["01", undefined, undefined, "am"])
-  t.deepEqual(parseTime("00"), ["00", undefined, undefined])
+  t.deepEqual(parseTime("01am"), [1, undefined, undefined, "am"])
+  t.deepEqual(parseTime("00"), [0, undefined, undefined])
 })
 
 test("Should return null on call without any arguments", t => {
